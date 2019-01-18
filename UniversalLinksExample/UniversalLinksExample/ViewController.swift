9407c904e9f0d8d76f8adbdb84cdf0a798329ef2
Caio Brigagão Lunardi <caio.lunardi@ynap.com>
2019-01-18 14:16:22 +0100

    Add base project
    Signed-off-by: Caio Brigagão Lunardi <caio.lunardi@ynap.com>

---
+++ UniversalLinksExample/UniversalLinksExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UniversalLinksExample
+//
+//  Created by Caio Brigagão Lunardi on 18/01/2019.
+//  Copyright © 2019 Caio Brigagão Lunardi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
